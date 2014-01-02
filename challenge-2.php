3f541c7e756df1352f08e7e556256c770b314150
Brandon Tomlinson <thebwt@gmail.com>
2014-01-01 18:40:23 -0600

    Got ssh keys working for challenege 2

--- challenge-2.php
+++ challenge-2.php
@@ -37,25 +37,72 @@ $pubkey = $_SERVER['HOME'] . '/' . '.ssh/id_rsa.pub';
 if (file_exists($pubkey)) #cred file is there
 {
     $file_handle = fopen($pubkey, 'r');
-    $line = explode(" ",fgets($file_handle));
-    $keyValue = $line[1];
-    $keyName = $line[0];
+    $keyValue = fgets($file_handle);
+    $keyName = "test-key";
 }
 else
 {
-
-    echo "Please enter your publickey name (ssh-rsa?; NO DEFAULT): ";
-    $keyName = trim(fgets(STDIN));
     echo "Please enter ssh public key: ";
     $keyValue = trim(fgets(STDIN));
 }
 
-$keypair = (array(
-    'name' => $keyName,
-    'publcKey' => $keyValue
-));
+);
 
+#create server
+$compute = $client->computeService('cloudServersOpenStack', 'ORD');
+$server = $compute->server();
 
+#really, this is the nicest way to do this, so copy pasta
+
+$server->addFile('/root/.ssh/authorized_keys', $keyValue);
+
+try {
+    $response = $server->create(array(
+        'name'     => 'Challenege-2',
+        'image'    => $compute->image('80fbcb55-b206-41f9-9bc2-2dd7aac6c061'),
+        'flavor'   => $compute->flavor('2'),
+        'networks' => array(
+            $compute->network(Network::RAX_PUBLIC),
+            $compute->network(Network::RAX_PRIVATE)
+                )
+    ));
+} catch (\Guzzle\Http\Exception\BadResponseException $e) {
+
+    // No! Something failed. Let's find out:
+
+    $responseBody = (string) $e->getResponse()->getBody();
+    $statusCode   = $e->getResponse()->getStatusCode();
+    $headers      = $e->getResponse()->getHeaderLines();
+
+    echo sprintf('Status: %s\nBody: %s\nHeaders: %s', $statusCode, $responseBody, implode(', ', $headers));
+}
+
+#wait for server to build and then get the ip address
+
+#more I can't do it more copy pasta
+$callback = function($server) {
+    if (!empty($server->error)) {
+        var_dump($server->error);
+        exit;
+    } else {
+        echo sprintf(
+            "Waiting on %s/%-12s %4s%%\n",
+            $server->name(),
+            $server->status(),
+            isset($server->progress) ? $server->progress : 0
+        );
+    }
+};
+
+$server->waitFor(ServerState::ACTIVE, 600, $callback);
+$server->refresh();
+echo sprintf(
+    "Admin Password: %s\nIPv4 Address: %s\n",
+    $server->adminPass,
+    $server->ip()
+);
+
+echo "\n";
 
 		
 ?>
